c85a11f7dd46de952e65a478e90e043bda3398c8
Soeren Etler <soeren.etler@de.ibm.com>
2017-02-15 19:57:50 +0100

    "Next" Button

--- brillianideas/quiz/controller.js
+++ brillianideas/quiz/controller.js
@@ -1,108 +1,140 @@
-$(document).ready(function () {
-	
-var questionNumber=0;
-var questionBank=new Array();
-var q=new Array();
-var stage="#game1";
-var stage2=new Object;
-var questionLock=false;
-var numberOfQuestions;
-var score=0;
- 
- 		$.getJSON('activity.json', function(data) {
+$(document)
+		.ready(
+				function() {
 
-		for(i=0;i<data.quizlist.length;i++){ 
-			questionBank[i]=new Array;
-			questionBank[i][0]=data.quizlist[i].question;
-			questionBank[i][1]=data.quizlist[i].option1;
-			questionBank[i][2]=data.quizlist[i].option2;
-			questionBank[i][3]=data.quizlist[i].option3;
-			questionBank[i][4]=data.quizlist[i].option4;
-			questionBank[i][5]=data.quizlist[i].option5;
-		}
-		 numberOfQuestions=questionBank.length; 
-		
-		 
-		displayQuestion();
-		})//gtjson
- 
- 
+					var questionNumber = 0;
+					var questionBank = new Array();
+					var q = new Array();
+					var stage = "#game1";
+					var stage2 = new Object;
+					var questionLock = false;
+					var numberOfQuestions;
+					var score = 0;
 
+					$.getJSON('activity.json', function(data) {
 
+						for (i = 0; i < data.quizlist.length; i++) {
+							questionBank[i] = new Array;
+							questionBank[i][0] = data.quizlist[i].question;
+							questionBank[i][1] = data.quizlist[i].option1;
+							questionBank[i][2] = data.quizlist[i].option2;
+							questionBank[i][3] = data.quizlist[i].option3;
+							questionBank[i][4] = data.quizlist[i].option4;
+							questionBank[i][5] = data.quizlist[i].option5;
+						}
+						numberOfQuestions = questionBank.length;
 
-function displayQuestion(){
+						displayQuestion();
+					})// gtjson
 
+					function displayQuestion() {
 
-var q=new Array();
-var contentArray =new Array();
-$(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</div>')
-for(i=1;i<questionBank[questionNumber].length;i++){
-	q[i-1]=questionBank[questionNumber][i];
-	
-	
-	contentArray[i-1] = '<div id='+ i +' class="option">'+q[i-1]+'</div>';	
-}
+						var q = new Array();
+						var contentArray = new Array();
+						$(stage).append(
+								'<div class="questionText">'
+										+ questionBank[questionNumber][0]
+										+ '</div>')
+						for (i = 1; i < questionBank[questionNumber].length; i++) {
+							q[i - 1] = questionBank[questionNumber][i];
 
-	shuffle(contentArray);
+							contentArray[i - 1] = '<div id=' + i
+									+ ' class="option">' + q[i - 1] + '</div>';
+						}
 
-	for(i=0;i<contentArray.length;i++){
-		$(stage).append(contentArray[i]);
-	}
-	
+						shuffle(contentArray);
 
- $('.option').click(function(){
-  if(questionLock==false){questionLock=true;	
-  //correct answer
-  if(this.id==1){
-   $(stage).append('<div class="feedback1">CORRECT</div>');
-   score++;
-   }
-  //wrong answer	
-  if(this.id!=1){
-	  $("#1").css('background-color', '#85ba1c');
-	  $(stage).append('<div class="feedback2">WRONG</div>');
-  }
-  setTimeout(function(){changeQuestion()},1000);
- }})
-}//display question
+						for (i = 0; i < contentArray.length; i++) {
+							$(stage).append(contentArray[i]);
+						}
 
-	
-	function changeQuestion(){
-		
-		questionNumber++;
-	
-	if(stage=="#game1"){stage2="#game1";stage="#game2";}
-		else{stage2="#game2";stage="#game1";}
-	
-	if(questionNumber<numberOfQuestions){displayQuestion();}else{displayFinalSlide();}
-	
-	 $(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
-	 $(stage).animate({"right": "+=800px"},"slow", function() {questionLock=false;});
-	}//change question
-	
+						$('.option')
+								.click(
+										function() {
+											if (questionLock == false) {
+												questionLock = true;
+												// correct answer
+												if (this.id == 1) {
+													$(stage)
+															.append(
+																	'<div class="feedback1">CORRECT</div>');
+													score++;
+												}
+												// wrong answer
+												if (this.id != 1) {
+													$("#1").css(
+															'background-color',
+															'#85ba1c');
+													$(stage)
+															.append(
+																	'<div class="feedback2">WRONG</div>');
+												}
+												$(stage)
+														.append(
+																'<div class="next">Next</div>');
+												$('.next').click(function(){
+													changeQuestion();
+												})
+											}
+										})
+					}// display question
 
-	
-	
-	function displayFinalSlide(){
-		
-		$(stage).append('<div class="questionText">You have finished the quiz!<br><br>Total questions: '+numberOfQuestions+'<br>Correct answers: '+score+'</div>');
-		
-	}//display final slide
-	
-	
-	/**
-	 * Shuffles array in place.
-	 * @param {Array} a items The array containing the items.
-	 */
-	function shuffle(a) {
-	    var j, x, i;
-	    for (i = a.length; i; i--) {
-	        j = Math.floor(Math.random() * i);
-	        x = a[i - 1];
-	        a[i - 1] = a[j];
-	        a[j] = x;
-	    }
-	}
+					function displayFinalSlide() {
 
-	
-	});//doc ready
+						$(stage)
+								.append(
+										'<div class="questionText">You have finished the quiz!<br><br>Total questions: '
+												+ numberOfQuestions
+												+ '<br>Correct answers: '
+												+ score + '</div>');
+
+					}// display final slide
+
+					/**
+					 * Shuffles array in place.
+					 * 
+					 * @param {Array}
+					 *            a items The array containing the items.
+					 */
+					function shuffle(a) {
+						var j, x, i;
+						for (i = a.length; i; i--) {
+							j = Math.floor(Math.random() * i);
+							x = a[i - 1];
+							a[i - 1] = a[j];
+							a[j] = x;
+						}
+					}
+					
+					function changeQuestion() {
+
+						questionNumber++;
+
+						if (stage == "#game1") {
+							stage2 = "#game1";
+							stage = "#game2";
+						} else {
+							stage2 = "#game2";
+							stage = "#game1";
+						}
+
+						if (questionNumber < numberOfQuestions) {
+							displayQuestion();
+						} else {
+							displayFinalSlide();
+						}
+
+						$(stage2).animate({
+							"right" : "+=800px"
+						}, "slow", function() {
+							$(stage2).css('right', '-800px');
+							$(stage2).empty();
+						});
+						$(stage).animate({
+							"right" : "+=800px"
+						}, "slow", function() {
+							questionLock = false;
+						});
+					}// change question
+
+				});// doc ready
