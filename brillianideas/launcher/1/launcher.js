d5b99e8f0008a417cfbf4ebb6b86d4e33c92ee51
Soeren Etler <soeren.etler@de.ibm.com>
2017-02-27 21:03:16 +0100

    Launcher 2/ minor changes for Launcher 1

--- brillianideas/launcher/1/launcher.js
+++ brillianideas/launcher/1/launcher.js
@@ -1,15 +1,14 @@
 var $floaty = $('.floaty');
 
-$floaty.on('mouseover click', function(e) {
+$floaty.on('mouseover click touchstart tap', function(e) {
   $floaty.addClass('is-active');
   e.stopPropagation();
 });
 
 $floaty.on('mouseout', function() {
   $floaty.removeClass('is-active');
-  console.log("Test");
 });
 
-$('.container').on('click', function() {
+$('.container').on('click touchstart tap', function() {
   $floaty.removeClass('is-active');
 });
