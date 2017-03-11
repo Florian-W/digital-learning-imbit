9872dd3a0264badf52a783722c4fa9ee4eba66f5
wusala01 <nick.london94@gmail.com>
2017-03-11 14:43:35 +0100

    New Startpage Style

--- brillianideas/brillianIDEAS/js/navigation.js
+++ brillianideas/brillianIDEAS/js/navigation.js
@@ -4,38 +4,8 @@
 
 /* TODO Config auslagern -> JSON, INI, XML -> zu klären */
 
-$(function(){
-	/**
-	 * 
-	 * @param left is navigation leftwards
-	 * @returns true if successfull
-	 */
-	function changePage(left){
-		if (typeof left != "boolean"){
-			return false;
-		}
-		var current = jQuery('.current');
-		var target = left ? current.prev() : current.next();
-		
-		if (target.length == 0){
-			return false;
-		}
-		
-		current.toggleClass('current');
-		target.toggleClass('current');
-		
-		jQuery.ajax({
-			url: target.find('a').attr('href')
-		}).done(function(data){
-			jQuery('#content').empty().unbind().append(data);
-		})
-		
-		
-		
-		return true;
-	}
-	
-	jQuery("body").on("swiperight",function(){
+$(window).on('load', function(){
+	jQuery(document).on("swiperight",function(){
 		console.log('User swiped right')
 		changePage(true);
 	}).on("swipeleft",function(){
@@ -55,6 +25,33 @@ $(function(){
 		url: jQuery('.home').find('a').attr('href')
 	}).done(function(data){
 		jQuery('#content').append(data);
-	})
+	});
+});
 
-})
+/**
+ * 
+ * @param left is navigation leftwards
+ * @returns true if successfull
+ */
+function changePage(left){
+	if (typeof left != "boolean"){
+		return false;
+	}
+	var current = jQuery('.current');
+	var target = left ? current.prev() : current.next();
+	
+	if (target.length == 0){
+		return false;
+	}
+	
+	current.toggleClass('current');
+	target.toggleClass('current');
+	
+	jQuery.ajax({
+		url: target.find('a').attr('href')
+	}).done(function(data){
+		jQuery('#content').empty().unbind().append(data);
+	})
+	
+	return true;
+}
