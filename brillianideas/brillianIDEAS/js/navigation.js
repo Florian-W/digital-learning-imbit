acf244d1d761df9755aeea7aef91638fc5454d2c
wusala01 <nick.london94@gmail.com>
2017-03-21 11:27:04 +0100

    resolve Conflict

--- brillianideas/brillianIDEAS/js/navigation.js
+++ brillianideas/brillianIDEAS/js/navigation.js
@@ -7,10 +7,6 @@
 var dotsspace;
 
 $(window).on('load', function(){
-<<<<<<< HEAD
-	jQuery(document).on("swiperight",function(e){
-		if($(e.target).is('p,a,img,h1,h2,h3,h4,h5,span')) return false;	
-=======
 	/**
 	* Added hash to navigate page
 	*/
@@ -25,7 +21,7 @@ $(window).on('load', function(){
 	}
 	
 	jQuery(document).on("swiperight",function(){
->>>>>>> refs/remotes/origin/master
+		if($(e.target).is('p,a,img,h1,h2,h3,h4,h5,span')) return false;	
 		changePage(true);
 	}).on("swipeleft",function(e){
 		if($(e.target).is('p,a,img,h1,h2,h3,h4,h5,span')) return false;	
