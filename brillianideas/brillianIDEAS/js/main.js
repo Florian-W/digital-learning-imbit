d7d8d2a211dfc53ce3a81ee84c37b110109ad6a2
wusala01 <nick.london94@gmail.com>
2017-03-23 09:07:46 +0100

    Made to Undo

--- brillianideas/brillianIDEAS/js/main.js
+++ brillianideas/brillianIDEAS/js/main.js
@@ -6,10 +6,6 @@
 $ = jQuery;
 
 var $display;
-var $currentPosition = window.location.length > 1 ?
-    window.location.hash.substr(1).split('.') : [];
-
-var $setup = true;
 
 // Document Ready Function
 var digitalLearning = function (file) {
@@ -34,101 +30,44 @@ var digitalLearning = function (file) {
     $display.width = $(window).width() - $display.xoffset;
     $display.height = $(window).height() - $display.yoffset;
 
-    //TRIGGER
-    $('#site').click(function (e) {
-        var target = $(e.target);
-        if (target.is(".flipcard .face.front")) {
-            target.parent().children('.back').css('display', 'block');
-            	//toggleClass(foo) adds foo as class attribute
-            target.parent().toggleClass('flipped');
-            	//toggle() shows/ hides element depending on current state
-            $('#backlayer').toggle();
-            $('.flipcard:not(.flipped), .axistext').fadeOut();
-            if(location.hash.indexOf(target.data('target')) != -1){
-                window.location = '#' + window.location.hash.substr(1).split('.').slice(0, -1).join('.');
-            } else {
-                window.location = window.location.hash.length > 1 ?
-                    '#' + window.location.hash.substr(1).split('.').concat(target.data('target')).join('.') :
-                    '#' + target.data('target');
-            }
-        } else if (target.is('.list > .learning, .list > .learning *')) {
-            var ltarget = target;
-            while (!ltarget.is('.list > .learning'))
-                ltarget = ltarget.parent();
-            var data = ltarget.data('target');
-            ltarget.parent().toggle();
-            ltarget.parent().siblings('.category').toggle();
-            ltarget.parent().siblings('[data-bind=' + data + ']').toggle();
-            if(location.hash.indexOf(target.data('target')) != -1){
-                window.location = '#' + window.location.hash.substr(1).split('.').slice(0, -1).join('.');
-            } else {
-                window.location = '#' + window.location.hash.substr(1).split('.').concat(ltarget.data('target')).join('.');
-            }
-        } else if (target.is('.face.back')) {
-            // Do Nothing
-        } else if (target.is('#backlayer')) {
-            $('.flipped').find('.selectionbody > .learning').hide();
-            $('.flipped').find('.list, .category').show();
-            $('.flipped').removeClass('flipped');
-            window.location = '#';
-            target.fadeOut();
-            $('.flipcard:not(.flipped), .axistext').fadeIn();
-        } else if (target.is('#grid')){ // skips startup animation if grid is clicked
-        	// two times finish to stop both animation queues
-        	$('#site').find('*').finish().finish();
-        } else if (target.is('.menuSubject')){
-        	$('.contentWrapper').hide().filter('[data-bind="' + target.data('target') + '"]').show();
-        	$('.menuSubject').removeClass('active');
-        	target.addClass('active');
-        }
-    });
 
     makeGrid(file);
 };
-/**
- *
- * @returns {$}
- */
-$.fn.center = function () {
-    this.css("position", "absolute");
-    this.css("top", Math.max(0, (
-            ($(window).height() - $(this).outerHeight()) / 2) +
-            $(window).scrollTop()) + "px"
-    );
-    this.css("left", Math.max(0, (
-            ($(window).width() - $(this).outerWidth()) / 2) +
-            $(window).scrollLeft()) + "px"
-    );
-    return this;
+
+var siteClick = function (e) {
+	var target = $(e.target);
+	if (target.is(".flipcard .face.front")) {
+		target.parent().toggleClass('flipped').children('.back').css('display', 'block');
+		$('#backlayer').toggle();
+		$('.flipcard:not(.flipped), .axistext').fadeOut();
+	} else if (target.is('.list > .learning, .list > .learning *')) {
+		var ltarget = target;
+		while (!ltarget.is('.list > .learning'))
+			ltarget = ltarget.parent();
+		var data = ltarget.data('target');
+		ltarget.parent().toggle()
+			.siblings('.category').toggle()
+			.siblings('[data-bind=' + data + ']').toggle();
+	} else if (target.is('.face.back')) {
+		// Do Nothing
+	} else if (target.is('#backlayer')) {
+		$('.flipped').find('.selectionbody > .learning').hide();
+		$('.flipped').find('.list, .category').show();
+		$('.flipped').removeClass('flipped');
+		target.fadeOut();
+		$('.flipcard:not(.flipped), .axistext').fadeIn();
+	} else if (target.is('#grid')){ // skips startup animation if grid is clicked
+		// two times finish to stop both animation queues
+		$('#site').find('*').finish().finish();
+	} else if (target.is('.menuSubject')){
+		$('.contentWrapper').hide().filter('[data-bind="' + target.data('target') + '"]').show();
+		$('.menuSubject').removeClass('active');
+		target.addClass('active');
+	}
 };
 
-/**
- * should be used for back and forth browser functions
- * should use URL to open/ close next/last (DOM) element
- */
-var walkToPath = function walkToPath(){
-    $newPosition = window.location.length > 1 ?
-        window.location.hash.substr(1).split('.') : [];
-    if ($currentPosition.length > $newPosition.length){
-        //go in
-    } else {
-        // go out
-    	// should enable back and forth browser functions
-    }
-};
+var initPage = function(){
+	
+}
 
-/**
- * opens (DOM) element or element chain according to URL
- */
-var openPath = function openPath(){
-    var path = window.location.hash.substr(1).split('.');
-    if (path.length > 0 && path[0] != ""){
-        var event = jQuery.Event('click');
-        var root = $('body');
-        for (var i = 0; path.length > i; i++){
-            event.target = root.find('[data-target='+path[i]+']')[0];
-            root = root.find('[data-bind=' + path[i] + ']').parent();
-            $('body').trigger(event);
-        }
-    }
-};
+$(document).ready(initPage)
--- brillianideas/brillianIDEAS/js/makeGrid.js
+++ brillianideas/brillianIDEAS/js/makeGrid.js
@@ -4,46 +4,6 @@
  */
 
 /**
- * Konfiguration für das Koordnatensystem inkl. X-Y-Koordinaten und die Anzeigereihenfolge
- * @contant {Array} digitalLearningArray
- */
-const digitalLearningArray = [
-	['mooc', 0.49034749 , 0.16136364 ,1],
-		['3dwelt', 0.5997426 , 0.05681818 ,2],
-		['chat', 0.615118662 , 0.25909091 ,3],
-		['forum', 0.82110682 , 0.25909091 ,4],
-		['sn', 0.8996139 , 0.22272727 ,5],
-		['twitter', 0.6988417 , 0.4 ,6],
-		['weblogs', 0.5456885 , 0.48636364 ,7],
-		['tagebuch', 0.70785071 , 0.53181818 ,8],
-		['wiki', 0.93693694 , 0.66136364 ,9],
-		['podcast', 0.94465894 , 0.75681818 ,10],
-		['ar', 0.92792793 , 0.86590909 ,11],
-		['film', 0.67181467 , 0.83181818 ,12],
-		['lod', 0.71943372 , 0.92727273 ,13],
-		['ebook', 0.52509653 , 0.91818182 ,14],
-		['sim', 0.49034749 , 0.8 ,15],
-		['spiel', 0.36550837 , 0.58181818 ,16],
-		['cbtwbt', 0.16344916 , 0.88863636 ,17],
-		['sg', 0.35392535 , 0.49772727 ,18],
-		['bl', 0.1003861 , 0.40909091 ,19],
-		//['webinar', 0.08880309 , 0.19318182 ,20], // Todo: somehow missing 
-		['vc', 0.21879022 , 0.23409091 ,21],
-		['ps', 0.38738739 , 0.28181818 ,22],
-		['iw', 0.35521236 , 0.075 ,23]
-		];
-
-var i;
-var j;
-var movedInCombo = new Array();
-for (i = 0; i < digitalLearningArray.length; i++){
-	movedInCombo[digitalLearningArray[i][0]] = new Array();
-	for (j = 0; j < digitalLearningArray.length; j++){
-		movedInCombo[digitalLearningArray[i][0]][digitalLearningArray[0][j]] = false;
-	}
-}
-
-/**
  * Sammelklasse für die Außendimensionen von Elementen des Koordinatensystems. Alle Koordinaten sind berechnet von der oberen linken Ecke des Dokuments.
  * @class rectOutlines
  * @param $div { Object } Die Flipcard (Koordinatensystem-Element), dessen Dimensionen berechnet werden sollen.
@@ -100,74 +60,71 @@ class rectOutlines{
 		return !(obj1.bottom <= obj2.top || obj2.bottom <= obj1.top || obj1.right <= obj2.left || obj2.right <= obj1.left ); 
 	}
 };
-/**
- * Verschiebt alle Elemente im Koordinatensystem, sodass sie sich nicht überschneiden.
- * @function noOverlayInGrid
- * @param id {String} ID des HTML-DOM-Node
- * @param x {Number} Ziel-X-Position des Elements 0 < x < 1
- * @param y {Number} Ziel-Y-Position des Elements 0 < y < 1
- * @param count {String | Number} Position in der Reihenfolge des Erscheinens
- * @param key {Number} Position im Konfigurationsarray
- * @author Nick London <nick.london94@gmail.com>
- */
-function noOverlayInGrid(id, x, y, count, key){
-	/**
-	 * Setze lokale Variablen
-	 */ 
-	var loop;
-	var $card = $('#' + id);
-	var arr = digitalLearningArray.slice(0, key);
-	var e_position;
-	var card_position;
-	
-	$front= $card.find('.front');
-	/**
-	 * Zeilenumbrüche verhindern
-	 */
-	$front.html($front.html().replace(' ', '&nbsp;').replace('-','&#8209;'));
-	/**
-	 * Initiale Positionierung
-	 */
-	$card.css({
-		width: $front.outerWidth(true),
-		height: $front.outerHeight(true),
-		left: Math.floor(x * $display.width) - $card.outerWidth(true) / 2,
-		top: Math.floor(y * $display.height) - $card.outerHeight(true) / 2
-	});
 
-	card_position = new rectOutlines($card);
-	
-	do {
+
+class Grid{
+	/**
+	 * Verschiebt alle Elemente im Koordinatensystem, sodass sie sich nicht überschneiden.
+	 * @function noOverlayInGrid
+	 * @param id {String} ID des HTML-DOM-Node
+	 * @param x {Number} Ziel-X-Position des Elements 0 < x < 1
+	 * @param y {Number} Ziel-Y-Position des Elements 0 < y < 1
+	 * @param count {String | Number} Position in der Reihenfolge des Erscheinens
+	 * @param key {Number} Position im Konfigurationsarray
+	 * @author Nick London <nick.london94@gmail.com>
+	 */
+	static noOverlayInGrid(id, x, y, count, key){
 		/**
-		 * Setze Schleifenvariablen für äußere Schleife
-		 */
-		loop = false;
+		 * Setze lokale Variablen
+		 */ 
+		var loop;
+		var $card = $('#' + id);
+		var arr = digitalLearningArray.slice(0, key);
+		var e_position;
+		var card_position;
+		var movedInCombo = new Array();
+		for (var i = 0; i < digitalLearningArray.length; i++){
+			movedInCombo[digitalLearningArray[i][0]] = new Array();
+			for (var j = 0; j < digitalLearningArray.length; j++){
+				movedInCombo[digitalLearningArray[i][0]][digitalLearningArray[0][j]] = false;
+			}
+		}
 		
-		$.each(arr, function(k, value){
-			/**
-			 * Setze Schleifenvariablen für innere Schleife (Value [0] entspricht der ID Position im Array
-			 */
-			e_position = new rectOutlines($('#' + value[0]));
-			
-			/**
-			 * check if both divs intersect
-			 */
-			if(rectOutlines.overlaps(e_position, card_position)){
-				if (movedInCombo[value[0]][id]){
-					card_position.left = ((card_position.left <= e_position.left)?e_position.right:e_position.left - card_position.width);
-					card_position.top = ((card_position.top <= e_position.top)? e_position.bottom : e_position.top - card_position.height);
+		var $front = $card.find('.front');
+		/**
+		 * Zeilenumbrüche verhindern
+		 */
+		$front.html($front.html().replace(' ', '&nbsp;').replace('-','&#8209;'));
+		/**
+		 * Initiale Positionierung
+		 */
+		$card.css({
+			width: $front.outerWidth(true),
+			height: $front.outerHeight(true),
+			left: Math.floor(x * $display.width) - $card.outerWidth(true) / 2,
+			top: Math.floor(y * $display.height) - $card.outerHeight(true) / 2
+		});
 
-					card_position.bottom = card_position.top + card_position.height;
-					card_position.right = card_position.left + card_position.width;
-					
-					$card.css({
-						left: card_position.left + "px",
-						top:  card_position.top + "px"
-					});
-					
-				} else {
-					card_position.left = ((card_position.left <= e_position.left)?e_position.left - card_position.width:e_position.right);
-					card_position.top = ((card_position.top <= e_position.top)? e_position.top - card_position.height : e_position.bottom);
+		card_position = new rectOutlines($card);
+		
+		do {
+			/**
+			 * Setze Schleifenvariablen für äußere Schleife
+			 */
+			loop = false;
+			
+			$.each(arr, function(k, value){
+				/**
+				 * Setze Schleifenvariablen für innere Schleife (Value [0] entspricht der ID Position im Array
+				 */
+				e_position = new rectOutlines($('#' + value[0]));
+				
+				/**
+				 * check if both divs intersect
+				 */
+				if(rectOutlines.overlaps(e_position, card_position)){
+					card_position.left = ((card_position.left <= e_position.left ^ movedInCombo[value[0]][id])?e_position.left - card_position.width:e_position.right);
+					card_position.top = ((card_position.top <= e_position.top ^ movedInCombo[value[0]][id])? e_position.top - card_position.height : e_position.bottom);
 
 					card_position.bottom = card_position.top + card_position.height;
 					card_position.right = card_position.left + card_position.width;
@@ -177,22 +134,115 @@ function noOverlayInGrid(id, x, y, count, key){
 						top:  card_position.top + "px"
 					});
 					movedInCombo[value[0]][id] = true;
+					/**
+					 * ensure loop continues and reset iteration through divs
+					 */
+					loop = true;
+					return false;
 				}
-				/**
-				 * ensure loop continues and reset iteration through divs
-				 */
-				loop = true;
-				return false;
+				return true;
+			})
+		} while(loop);
+		/**
+		 * set data value for orde of appearance
+		 */
+		$card.attr('data-sid', "" + count);
+		return $.Deferred().promise();
+	};
+			
+	static cleanUp(){
+		$('#grid').css('cursor', 'default');
+		$('.flipcard, .flipcard .face').css('pointer-events', 'auto').css('cursor', 'pointer');
+		$('h1, h2, h3, h4 ,h5, p').each(function(i,e){
+			$(e).html(($(e).html().replace(/\s{2,}/g," ")));
+		});
+		$('.flipcard').each(function(i,e){$(e).css({width: $(e).children('.front').outerWidth(true) +1, height: $(e).children('.front').outerHeight(true)+1})})
+		$('.flipcard').each(function(i,e){$(e).css({minWidth: $(e).children('.front').outerWidth(true) +1, minHeight: $(e).children('.front').outerHeight(true)+1})})
+	}
+	
+	static sortTiles(a, b) {
+			return (($(a).data('sid') > $(b).data('sid')) ? 1 : -1);
+		};
+	
+	static animateTile(obj){
+		var $obj = $(obj);
+		Grid.animationPromises.push($obj.animate({opacity: 1}, {
+			duration: 500, 
+			done: (($obj.next().length == 0) ? undefined : function(){
+				Grid.animateTile($obj.next());
+			})
+		}));
+	}
+	
+	static animateTiles() {
+		/**
+		 * disables the tiles and enables the back layer
+		 */
+		$('#grid').css('cursor', 'pointer').css("width", $display.width).css("height", $display.height).css('opacity', 1);
+		$('.flipcard, .flipcard .face').css('pointer-events', 'none').css('cursor', 'default');
+
+		$('#animation_welcome').animate({left: 50 + $('#animation_welcome').outerWidth() / 2, top: 100}, {duration: 1000})
+		$('#xaxis').animate({opacity: 1, width: $display.width}, {duration: 1000});
+		$('#yaxis').animate({opacity: 1, height: $display.height}, {duration: 1000});
+		
+		$.each(digitalLearningArray, function(key, value){
+			var v = value.slice();
+			v.push(key);
+			Grid.noOverlayInGrid.apply({}, v); // {@link noOverlayInGrid}
+		});
+		
+		var $flipcards = $('#grid').children('.flipcard');
+		$flipcards.detach();
+		$flipcards.sort(Grid.sortTiles);
+		$('#grid').html($flipcards).append('<div id="backlayer"></div>');
+		$flipcards = $('#grid').children('.flipcard');
+		Grid.animateTile($flipcards.first());
+		
+		$.when(Grid.animationPromises).done(Grid.cleanUp);
+	}
+	
+	static loadLearnings(obj){
+		var $obj = $(obj);
+		$.ajax({
+			url: 'xml/index.php?base=learning&withLink=false&type=' + $obj.attr('id'), 
+			complete: function (data) {
+				$obj.find('.list').append(data.responseText);
+				$obj.find('.list').children().each(function (index, element) {
+					$.ajax({
+						url: 'xml/index.php?base=learning&withLink=true&detail=true&guid=' + $(element).data('target'), 
+						complete: function (learningData) {
+							var $newElement = $obj.find('.list').parent().append(learningData.responseText).children('.learning');
+							$newElement.fadeOut();
+							$newElement.html($newElement.html().replace(/(?:\r\n|\r|\n)/g," "));
+						}
+					});
+				});
 			}
-			return true;
-		})
-	} while(loop);
-	/**
-	 * set data value for orde of appearance
-	 */
-	$card.attr('data-sid', "" + count);
-	return $.Deferred().promise();
-};
+		});
+	}
+	
+	static fillTile(i, obj){
+		var $obj = $(obj);
+		$.ajax({
+			url: 'xml/index.php?base=categories&type=learning&detail=true&filter=' + $obj.attr("id"),
+			complete: function(data){
+				$obj.children('.back').append(data.responseText);
+				Grid.loadLearnings($obj);
+			}
+		});
+	}
+	
+	static fillTiles(){
+		$(document).ajaxStop(function() {
+			Grid.animateTiles();
+			 $(this).unbind("ajaxStop");
+		});
+		$('#grid').children('.flipcard').each(Grid.fillTile);
+	}
+}
+
+Grid.animationPromises = new Array();
+
 
 /**
  * Does all the start animation an utilizes the position function noOVerlayGrid
@@ -201,120 +251,22 @@ function noOverlayInGrid(id, x, y, count, key){
  * @returns {undefined} nothing
  * @author Nick London <nick.london94@gmail.com>
  */
-var makeGrid = function makeGrid(view){
+function makeGrid(view){
+	 $('#site').on("click", siteClick);
 	/**
 	 * decides the view
 	 */
     switch (view){
         case 'digitalLearning':
-			var animationPromises = new Array();
-			
-			var cleanUp = function(){
-				$('#grid').css('cursor', 'default');
-				$('.flipcard, .flipcard .face').css('pointer-events', 'auto').css('cursor', 'pointer');
-				$('h1, h2, h3, h4 ,h5, p').each(function(i,e){
-					$(e).html(($(e).html().replace(/\s{2,}/g," ")));
-				});
-				$('.flipcard').each(function(i,e){$(e).css({width: $(e).children('.front').outerWidth(true) +1, height: $(e).children('.front').outerHeight(true)+1})})
-				$('.flipcard').each(function(i,e){$(e).css({minWidth: $(e).children('.front').outerWidth(true) +1, minHeight: $(e).children('.front').outerHeight(true)+1})})
-			}
-			
-			var sortTiles = function (a, b) {
-					return (($(a).data('sid') > $(b).data('sid')) ? 1 : -1);
-				};
-			
-			var animateTile = function(obj){
-				var $obj = $(obj);
-				animationPromises.push($obj.animate({opacity: 1}, {
-					duration: 500, 
-					done: (($obj.next().length == 0) ? undefined : function(){
-						animateTile($obj.next());
-					})
-				}));
-			}
-			
-			var animateTiles = function () {
-				
-				/**
-				 * disables the tiles and enables the back layer
-				 */
-				$('#grid').css('cursor', 'pointer');
-				$('.flipcard, .flipcard .face').css('pointer-events', 'none').css('cursor', 'default');
-				$('#grid').css("width", $display.width).css("height", $display.height);
-				$('#grid').css('opacity', 1);
-				$('#animation_welcome').animate({left: 50 + $('#animation_welcome').outerWidth() / 2, top: 100}, {duration: 1000})
-				$('#xaxis').animate({opacity: 1, width: $display.width}, {duration: 1000});
-				$('#yaxis').animate({opacity: 1, height: $display.height}, {duration: 1000});
-				
-				
-				$.each(digitalLearningArray, function(key, value){
-					var v = value.slice();
-					v.push(key);
-					noOverlayInGrid.apply({}, v); // {@link noOverlayInGrid}
-				});
-				
-				
-				var $flipcards = $('#grid').children('.flipcard');
-				$flipcards.detach();
-				$flipcards.sort(sortTiles);
-				$('#grid').html($flipcards).append('<div id="backlayer"></div>');
-				$flipcards = $('#grid').children('.flipcard');
-				animateTile($flipcards.first());
-				
-				
-				$.when(animationPromises).done(cleanUp)
-			
-			}
-			
-			var loadLearnings = function(obj){
-				var $obj = $(obj);
-				$.ajax({
-					url: 'xml/index.php?base=learning&withLink=false&type=' + $obj.attr('id'), 
-					complete: function (data) {
-						$obj.find('.list').append(data.responseText);
-						$obj.find('.list').children().each(function (index, element) {
-							$.ajax({
-								url: 'xml/index.php?base=learning&withLink=true&detail=true&guid=' + $(element).data('target'), 
-								complete: function (learningData) {
-									var $newElement = $obj.find('.list').parent().append(learningData.responseText).children('.learning');
-									$newElement.fadeOut();
-									$newElement.html($newElement.html().replace(/(?:\r\n|\r|\n)/g," "));
-								}
-							});
-						});
-					}
-				});
-			}
-			
-			var fillTile = function(i, obj){
-				var $obj = $(obj);
-				$.ajax({
-					url: 'xml/index.php?base=categories&type=learning&detail=true&filter=' + $obj.attr("id"),
-					complete: function(data){
-						$obj.children('.back').append(data.responseText);
-						loadLearnings($obj);
-					}
-				});
-			}
-			
-			var fillTiles = function(){
-				$(document).ajaxStop(function() {
-					animateTiles();
-					 $(this).unbind("ajaxStop");
-				});
-				$('#grid').children('.flipcard').each(fillTile);
-			}	      
-						
             /**
              * loads all tiles and displays the heading
              */
-			 
 			$('#animation_welcome').animate({opacity: 1}, {duration: 1000}),
 			$.ajax({
 				url:'xml/index.php?base=grid&type=learning',
 				complete: function (data) {
 					if(data.status == 200){
-						$.when($('#site').append(data.responseText)).done(fillTiles);
+						$.when($('#site').append(data.responseText)).done(Grid.fillTiles);
 					} else {
 						alert('Something went wrong. Please reload this page later. If this keeps occuring, please contact the mail stated in the imprint.');
 					}
