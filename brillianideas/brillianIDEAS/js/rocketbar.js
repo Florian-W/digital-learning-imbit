ad82ff08f0d0f4942a2b5a2214f0df1771cf781f
wusala01 <nick.london94@gmail.com>
2017-03-27 08:24:33 +0200

    Update rocketbar.js

--- brillianideas/brillianIDEAS/js/rocketbar.js
+++ brillianideas/brillianIDEAS/js/rocketbar.js
@@ -86,7 +86,7 @@ expstickybar.prototype={
 			thisbar.$stickybar.velocity({
 				bottom: '-' + $('.content').outerHeight() 
 			}, thisbar.setting.speed * (thisbar.$stickybar.outerHeight() / $('.content').outerHeight() - 1), "swing", function(){
-				thisbar.$stickybar.css('visibility', 'visible');
+				(thisbar.showhide("hide")).bind(thisbar)
 				($('.current').hasClass('home')) ? (thisbar.toggle).bind(thisbar) : undefined;
 			});
 		});
