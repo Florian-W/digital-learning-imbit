5dc2c285f35055ff684af07f24a19dc35e1ab101
Tim Häusler <timotius97@googlemail.com>
2018-03-01 12:58:57 +0100

    Updated Bakery

--- brillianicm/src/org/dhbw/imbit11/backend/BadgeBakery.java
+++ brillianicm/src/org/dhbw/imbit11/backend/BadgeBakery.java
@@ -23,39 +23,30 @@ public class BadgeBakery extends HttpServlet {
 		switch (country){
 			case "Brazil": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_brazil.svg";
-				svgString = svgReader.toString();
 				break;
 			case "Spain": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_spain.svg";
-				svgString = svgReader.toString();
 				break;
 			case "China": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_china.svg";
-				svgString = svgReader.toString();
 				break;
 			case "USA": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_USA.svg";
-				svgString = svgReader.toString();
 				break;
 			case "Sweden": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_sweden.svg";
-				svgString = svgReader.toString();
 				break;
 			case "India": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_india.svg";
-				svgString = svgReader.toString();
 				break;
 			case "Germany": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_germany.svg";
-				svgString = svgReader.toString();
 				break;	
 			case "Turkey": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_turkey.svg";
-				svgString = svgReader.toString();
 				break;
 			case "Australia": 
 				fileLocation = "/etc/opt/tomcat/webapps/brillianICM/WebContent/...badge_australia.svg";
-				svgString = svgReader.toString();
 				break;
 		}
 		FileReader svgReader = new FileReader(fileLocation);
